7ffd451bf8cdd6211f88fde8bdf54a346249e1f1
rosalinasitumorang02-create <rosalinasitumorang02@gmail.com>
2026-05-22 10:38:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 Aplikasi Rosa")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+st.image("sunrise.jpg", caption="Sunrise by the mountains")
