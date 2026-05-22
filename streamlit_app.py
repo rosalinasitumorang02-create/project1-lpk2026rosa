dedb828c51b7330e79c3867657f7323b331e34ae
rosalinasitumorang02-create <rosalinasitumorang02@gmail.com>
2026-05-22 10:43:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 import streamlit as st
-st.text("This is text\n[and more text](that's not a Markdown link).")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
