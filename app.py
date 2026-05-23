34cfb50e58b4f8e05e79e4e119f13031718b1e58
rosalinasitumorang02-create <rosalinasitumorang02@gmail.com>
2026-05-23 11:18:20 +0700

    Update and rename streamlit_app.py to app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Kuliah Praktisi 2305",
+  page_icon="🧊",
+  layout="centered",
+  initial_sidebar_state="expanded"
+)
+
+# Hirarki teks
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
+st.write("Hello, *World!* 😁")
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
+
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+st.button("Reset", type="primary")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
--- streamlit_app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.title("SUNGJAE GIRLFRIEND")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
