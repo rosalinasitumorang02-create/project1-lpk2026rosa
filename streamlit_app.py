48d01d441e248b2d8ef4a9920d6a453b2a576228
rosalinasitumorang02-create <rosalinasitumorang02@gmail.com>
2026-05-23 10:07:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
 import streamlit as st
-
-st.title("🎈 Aplikasi Rosa")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-import streamlit as st
-
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+st.title("SUNGJAE GIRLFRIEND")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
